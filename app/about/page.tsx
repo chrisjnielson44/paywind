7525b3ea4c0badc0b49cce3d46b6985942232664
Christopher Nielson <61611198+chrisjnielson44@users.noreply.github.com>
2023-09-28 00:47:27 -0400

    Revamped homepage

---
+++ app/about/page.tsx
@@ -0,0 +1,13 @@
+import Header from '../Components/header'
+import Body from './Body'
+import Footer from '../Components/footer'
+
+export default function Home() {
+    return (
+      <main className="">
+        <Header/>
+        <Body/>
+        <Footer/>
+      </main>
+    )
+  }
---
+++ app/prototype/app/page.tsx
@@ -0,0 +1,10 @@
+import App from "./Components/app"
+// import Footer from "../Components/footer"
+export default function Home() {
+    return (
+      <main className="">
+        <App/>
+        {/* <Footer/> */}
+      </main>
+    )
+  }

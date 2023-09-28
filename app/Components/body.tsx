b91342c6d28c2ae1b77fba2977549d4c9482634c
Christopher Nielson <61611198+chrisjnielson44@users.noreply.github.com>
2023-09-28 00:51:14 -0400

    apos error fixed

--- app/Components/body.tsx
+++ app/Components/body.tsx
@@ -30,7 +30,7 @@ export default function Body() {
                         <img className="" src="plaid.svg" alt="office content 1" />
                     </div>
                     <div className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
-                        <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">We didn't reinvent the wheel</h2>
+                        <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">We didn&apos;t reinvent the wheel</h2>
                         <p className="mb-4">We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need. Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need.</p>
                         <p>We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick.</p>
                     </div>

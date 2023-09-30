cca49939dd4ec0b3cada02fe9cf069f846a68ffc
Christopher Nielson <61611198+chrisjnielson44@users.noreply.github.com>
2023-09-29 20:27:38 -0400

    Updated About Me

--- app/about/Body.tsx
+++ app/about/Body.tsx
@@ -6,7 +6,7 @@ export default function Body() {
                 <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6 ">
                     <div className="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
                         <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Our Team</h2>
-                        <p className="font-light text-gray-500 lg:mb-16 sm:text-xl dark:text-gray-400">Explore the whole collection of open-source web components and elements built with the utility classes from Tailwind</p>
+                        <p className="font-light text-gray-500 lg:mb-16 sm:text-xl dark:text-gray-400"></p>
                     </div>
                     <div className="grid gap-8 mb-6 lg:mb-16 md:grid-cols-1">
                         <div className="items-center bg-gray-50 rounded-lg shadow sm:flex dark:bg-gray-800 dark:border-gray-700">
@@ -18,7 +18,7 @@ export default function Body() {
                                     <a href="#">Christopher Nielson</a>
                                 </h3>
                                 <span className="text-gray-500 dark:text-gray-400">CEO & Web Developer</span>
-                                <p className="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400">Bonnie drives the technical strategy of the flowbite platform and brand.</p>
+                                <p className="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400"></p>
                                 <ul className="flex space-x-4 sm:mt-0">
                                 <li>
                                 <a href="https://github.com/chrisjnielson44" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-gray-900 dark:text-white dark:hover:text-gray-400">

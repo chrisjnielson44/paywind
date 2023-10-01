ff98e42159eb62a248ad1bf981967725d25ed51e
Christopher Nielson <61611198+chrisjnielson44@users.noreply.github.com>
2023-10-01 17:07:53 -0400

    Added AWS Backend

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "paywind": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "CreatedSNSRole": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s3paywindstorage7bcf0a33": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}

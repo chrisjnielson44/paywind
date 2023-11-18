2e7332bb81ebcb250aabe2552f2be7b47c56bdb0
Christopher Nielson <61611198+chrisjnielson44@users.noreply.github.com>
2023-11-17 19:24:57 -0500

    Test

--- app/about/Body.tsx
+++ app/about/Body.tsx
@@ -5,20 +5,65 @@ export default function Body() {
         <main className="">
             <section className="h-screen bg-white dark:bg-gray-900">
                 <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6 ">
-                    <div className="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
-                        <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Our Team</h2>
+                    <div className="mx-auto max-w-screen-xl text-center mb-8 lg:mb-16">
+                        <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">About Us</h2>
                         <p className="font-light text-gray-500 lg:mb-16 sm:text-xl dark:text-gray-400"></p>
                     </div>
+
                     <div className="grid gap-8 mb-6 lg:mb-16 md:grid-cols-1">
+                    <h2 className="mb-1 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Mission</h2>
+                    <p className="text-xl ">
+                    At Paywind, we're passionate about revolutionizing personal finance management. 
+                    Our mission is to empower users with an intuitive and comprehensive financial hub that simplifies and enhances the way you handle your finances. 
+                    Our app, designed with a user-centric approach, offers a modular design allowing for a personalized experience that aligns with your unique financial goals. 
+                    By integrating innovative technology with insightful analytics, Paywind ensures that you're always in command of your financial journey, offering seamless budgeting, robust security, and efficient account management. 
+                    We're committed to making personal finance not just easier, but also a more insightful and growth-oriented experience. Join us at Paywind, where your financial wellness is our priority, and experience the future of personal finance management.
+                    </p>
+
+                    </div>
+
+                    <div className="grid gap-8 mb-6 lg:mb-16 md:grid-cols-1">
+                    <h2 className="mb-1 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Our Team</h2>
+
                         <div className="items-center bg-gray-50 rounded-lg shadow sm:flex dark:bg-gray-800 dark:border-gray-700">
                             <a href="#">
-                                <Image className="w-full rounded-lg sm:rounded-none sm:rounded-l-lg grayscale" src="smallprof.png" alt="Bonnie Avatar"/>
+                                <img width={150} height={150} className="rounded-lg sm:rounded-none sm:rounded-l-lg grayscale" src="LinkedInPhotoCJN.png" alt="Bonnie Avatar"/>
                             </a>
                             <div className="p-5">
                                 <h3 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
                                     <a href="#">Christopher Nielson</a>
                                 </h3>
-                                <span className="text-gray-500 dark:text-gray-400">CEO & Web Developer, Student</span>
+                                <span className="text-gray-500 dark:text-gray-400">Founder, Full Stack Web Developer, Student</span>
+                                <p className="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400"></p>
+                                <ul className="flex space-x-4 sm:mt-0">
+                                <li>
+                                <a href="" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-gray-900 dark:text-white dark:hover:text-gray-400">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="w-auto h-8 " fill="currentColor" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" /></svg>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-gray-900 dark:text-white dark:hover:text-gray-400">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="w-auto h-8 " fill="currentColor" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" /></svg>
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-gray-900 dark:text-white dark:hover:text-gray-400">
+                                    <svg className="w-auto h-8" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" /></svg>
+                                </a>
+                            </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="items-center bg-gray-50 rounded-lg shadow sm:flex dark:bg-gray-800 dark:border-gray-700">
+                            <a href="#">
+                                <img className="w-full rounded-lg sm:rounded-none sm:rounded-l-lg grayscale" src="" alt="Photo here"/>
+                            </a>
+                            <div className="p-5">
+                                <h3 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                    <a href="#">Tyler McKibbin</a>
+                                </h3>
+                                <span className="text-gray-500 dark:text-gray-400">Co Founder</span>
                                 <p className="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400"></p>
                                 <ul className="flex space-x-4 sm:mt-0">
                                 <li>
--- app/frontpage-components/footer.tsx
+++ app/frontpage-components/footer.tsx
@@ -14,7 +14,7 @@ export default function Footer() {
                     <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
                      <div>
                             <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Pages</h2>
-                            <ul className="text-gray-600 dark:text-gray-400">
+                            <ul className="text-gray-600 dark:text-gray-300">
                             <li className="mb-4">
                                     <a href="/dashboard" className="hover:underline ">Log In</a>
                             </li>
@@ -26,7 +26,7 @@ export default function Footer() {
                         </div>
                         <div>
                             <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Contact</h2>
-                            <ul className="text-gray-600 dark:text-gray-400">
+                            <ul className="text-gray-600 dark:text-gray-300">
                                 <li className="mb-4">
                                     <a href="mailto:cjnielson44@gmail.com" target="_blank" rel="noopener noreferrer" className="hover:underline ">Email</a>
                                 </li>
@@ -36,7 +36,7 @@ export default function Footer() {
 
                         <div>
                             <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Legal</h2>
-                            <ul className="text-gray-600 dark:text-gray-400">
+                            <ul className="text-gray-600 dark:text-gray-300">
                                 <li className="mb-4">
                                     <a href="#" className="hover:underline">Privacy Policy</a>
                                 </li>
@@ -50,13 +50,13 @@ export default function Footer() {
                 <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
 
                 <div className="sm:flex sm:items-center sm:justify-between">
-                    <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 PayWind. All Rights Reserved.
+                    <span className="text-sm text-gray-500 sm:text-center dark:text-gray-300">© 2023 PayWind. All Rights Reserved.
                     </span>
                     <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
                         {/* <a href="https://www.youtube.com/@ReMinFanMedia" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
                                 <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" viewBox="-35.20005 -41.33325 305.0671 247.9995"><path fillRule="evenodd" d="M229.763 25.817c-2.699-10.162-10.65-18.165-20.747-20.881C190.716 0 117.333 0 117.333 0S43.951 0 25.651 4.936C15.554 7.652 7.602 15.655 4.904 25.817 0 44.237 0 82.667 0 82.667s0 38.43 4.904 56.85c2.698 10.162 10.65 18.164 20.747 20.881 18.3 4.935 91.682 4.935 91.682 4.935s73.383 0 91.683-4.935c10.097-2.717 18.048-10.72 20.747-20.88 4.904-18.422 4.904-56.851 4.904-56.851s0-38.43-4.904-56.85" fill="currentColor"/><path d="M93.333 117.558l61.334-34.89-61.334-34.893z" fill="#fff"/></svg>
                             </a> */}
-                        <a href="https://www.instagram.com/reminfanmedia/" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+                        <a href="https://www.instagram.com/reminfanmedia/" target="_blank" rel="noopener noreferrer" className="text-gray-300 hover:text-gray-900 dark:hover:text-white">
                             <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fill-rule="evenodd" d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z" clip-rule="evenodd" /></svg>
                         </a>
                         {/* <a href="https://www.tiktok.com/@reminfanmedia" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
@@ -65,7 +65,7 @@ export default function Footer() {
                         {/* <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
                                 <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fill-rule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clip-rule="evenodd" /></svg>
                             </a> */}
-                        <a href="https://twitter.com/reminfanmedia" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+                        <a href="https://twitter.com/reminfanmedia" target="_blank" rel="noopener noreferrer" className="text-gray-300 hover:text-gray-900 dark:hover:text-white">
                             <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" /></svg>
                         </a>
                         {/* <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">

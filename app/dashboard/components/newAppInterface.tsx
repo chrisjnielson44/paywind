c2a964bd48af29cb2fa22600e711fd363affeae3
Christopher Nielson <61611198+chrisjnielson44@users.noreply.github.com>
2023-11-29 08:24:07 -0500

    Changed menu bar to white in dark mode

--- app/dashboard/components/newAppInterface.tsx
+++ app/dashboard/components/newAppInterface.tsx
@@ -243,7 +243,7 @@ const NewAppInterface: React.FC<AppInterfaceProps> = ({ userEmail, userName, pro
                 <div className="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-500 bg-gray-900 px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8">
                     <button type="button" className="-m-2.5 p-2.5 text-gray-700 lg:hidden" onClick={() => setSidebarOpen(true)}>
                         <span className="sr-only">Open sidebar</span>
-                        <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+                        <Bars3Icon className="text-white h-6 w-6" aria-hidden="true" />
                     </button>
 
                     {/* Separator */}

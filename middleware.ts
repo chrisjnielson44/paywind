f8b5b747eb57b1d6665b1da6abcd530ec8edd79c
Christopher Nielson <61611198+chrisjnielson44@users.noreply.github.com>
2023-12-17 23:24:36 +0100

    update

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,3 @@
-// export {default} from 'next-auth/middleware'
+export {default} from 'next-auth/middleware'
 
-// export const config = {matcher: ['/dashboard/:path*']}
-
-import { NextRequest, NextResponse } from 'next/server'
-
-export function middleware(req: NextRequest) {
-  // If the request is not for the root page, redirect to the root page
-  if (req.nextUrl.pathname !== 'https://paywind.io/') {
-    return NextResponse.redirect('https://paywind.io/')
-  }
-
-  // If the request is for the root page, continue as normal
-  return NextResponse.next()
-}
+export const config = {matcher: ['/dashboard/:path*']}

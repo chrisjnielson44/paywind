56020a1f4619e169bee21503b923583adf128482
Christopher Nielson <61611198+chrisjnielson44@users.noreply.github.com>
2023-12-17 23:13:58 +0100

    test

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,15 @@
-export {default} from 'next-auth/middleware'
+// export {default} from 'next-auth/middleware'
 
-export const config = {matcher: ['/dashboard/:path*']}
+// export const config = {matcher: ['/dashboard/:path*']}
+
+import { NextResponse } from 'next/server'
+
+export function middleware(req: { nextUrl: { pathname: string } }) {
+  // If the request is not for the root page, redirect to the root page
+  if (req.nextUrl.pathname !== 'https://paywind.io') {
+    return NextResponse.redirect('https://paywind.io')
+  }
+
+  // If the request is for the root page, continue as normal
+  return NextResponse.next()
+}

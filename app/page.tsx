b64a358045f4e44c4def12425eec6d06e5fd34bd
Christopher Nielson <61611198+chrisjnielson44@users.noreply.github.com>
2024-02-27 00:10:19 -0500

    Remove unused components from Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,9 @@
-import Header from './frontpage-components/header'
-import Body from './frontpage-components/body'
-import Footer from './frontpage-components/footer'
 import TempPage from './TempPage'
 
 export default function Home() {
   return (
     <main className="">
-      {/* <Header/>
-      <Body/>
-      <Footer/> */}
+
       <TempPage/>
     </main>
   )
